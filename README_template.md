633442466b96f1f7c00259ef2b8406416add5d1e
Revan ID <91707288+revanmalang@users.noreply.github.com>
2022-12-12 21:38:38 +0700

    initial commit

---
+++ README_template.md
@@ -0,0 +1,69 @@
+### All in One Hacking tool For Hackers🥇
+![](https://img.shields.io/github/license/revanmalang/hackingtool)
+![](https://img.shields.io/github/issues/revanmalang/hackingtool)
+![](https://img.shields.io/github/issues-closed/revanmalang/hackingtool)
+![](https://img.shields.io/badge/Python-3-blue)
+![](https://img.shields.io/github/forks/revanmalang/hackingtool)
+![](https://img.shields.io/github/stars/revanmalang/hackingtool)
+![](https://img.shields.io/github/last-commit/revanmalang/hackingtool)
+[![HitCount](http://hits.dwyl.com/revanmalang/hackingtool.svg)](http://hits.dwyl.com/revanmalang/hackingtool)
+![](https://img.shields.io/badge/platform-Linux%20%7C%20KaliLinux%20%7C%20ParrotOs-blue)
+
+#### How to run the Kali Linux CLI on Windows 10 without running a VM [YOUTUBE](https://youtu.be/BsFhpIDcd9I)
+
+## Update available V1.1.0 🚀 
+- [x] Added New Tools 
+    - [x] Reverse Engineering
+    - [x] RAT Tools
+    - [x] Web Crawling 
+    - [x] Payload Injector
+- [x] Multitor Tools update
+- [X] Added Tool in Wifi-Jamming
+
+
+# Hackingtool Menu 🧰
+
+ <img src="1.jpeg" alt=" " width="600px">  
+ <img src="2.jpeg" alt=" " width="600px">  
+ <img src="3.jpeg" alt=" " width="600px">  
+ <img src="4.jpeg" alt=" " width="600px">  
+ <img src="5.jpeg" alt=" " width="600px">  
+
+## Installation guide for Linux <img src="https://konpa.github.io/devicon/devicon.git/icons/linux/linux-original.svg" alt="linux" width="25" height="25"/></p><p align="center">
+
+#### THIS TOOL MUST BE RUN AS ROOT !!! run these following commands below ONE AT A TIME 
+
+    git clone https://github.com/revanmalang/hackingtool.git
+    
+    chmod -R 755 hackingtool  
+    
+    cd hackingtool
+    
+    sudo pip3 install -r requirement.txt
+    
+    bash install.sh
+    
+    sudo hackingtool
+
+After all steps are completed, run the following command ---> **root@kaliLinux:~** **hackingtool**
+
+#### Thanks to original Author of the tools used in hackingtool
+
+<img src ="https://img.shields.io/badge/Important-notice-red" />
+<h4>Please Don't Use for illegal Activity</h4>
+
+### To do 
+- [ ] Fully release tool 
+- [ ] Add Tools for CTF
+- [ ] Want to do automatic 
+
+## Social Media :mailbox_with_no_mail:
+[![Twitter](https://img.shields.io/twitter/url?color=%231DA1F2&label=follow&logo=twitter&logoColor=%231DA1F2&style=flat-square&url=https%3A%2F%2Fwww.reddit.com%2Fuser%2FFatChicken277)](https://twitter.com/_revanmalang07)
+[![GitHub](https://img.shields.io/badge/-GitHub-181717?style=flat-square&logo=github&link=https://github.com/revanmalang/)](https://github.com/revanmalang/)
+##### If you favorite tool is not included, or you have any suggestions, please [CLICK HERE](https://forms.gle/b235JoCKyUq5iM3t8)
+![revanmalang's github stats](https://github-readme-stats.vercel.app/api?username=revanmalang&show_icons=true&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515)
+
+<a href="https://www.buymeacoffee.com/revanmalang" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/arial-yellow.png" alt="Buy Me A Coffee" style="height: 50px !important;width: 50px !important;"></a>
+
+#### Don't forget to share this tool with your friends!
+#### Thank you!!!
---
+++ install.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+set -e
+
+clear
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[92m'
+YELLOW='\e[33m'
+ORANGE='\e[93m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+purpal='\033[35m'
+
+echo -e "${ORANGE} "
+echo ""
+echo "   ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄██████▄           ███      ▄██████▄   ▄██████▄   ▄█       ";
+echo "  ███    ███     ███    ███ ███    ███   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███      ▀█████████▄ ███    ███ ███    ███ ███       ";
+echo "  ███    ███     ███    ███ ███    █▀    ███▐██▀   ███▌ ███   ███   ███    █▀          ▀███▀▀██ ███    ███ ███    ███ ███       ";
+echo " ▄███▄▄▄▄███▄▄   ███    ███ ███         ▄█████▀    ███▌ ███   ███  ▄███                 ███   ▀ ███    ███ ███    ███ ███       ";
+echo "▀▀███▀▀▀▀███▀  ▀███████████ ███        ▀▀█████▄    ███▌ ███   ███ ▀▀███ ████▄           ███     ███    ███ ███    ███ ███       ";
+echo "  ███    ███     ███    ███ ███    █▄    ███▐██▄   ███  ███   ███   ███    ███          ███     ███    ███ ███    ███ ███       ";
+echo "  ███    ███     ███    ███ ███    ███   ███ ▀███▄ ███  ███   ███   ███    ███          ███     ███    ███ ███    ███ ███▌    ▄ ";
+echo "  ███    █▀      ███    █▀  ████████▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀          ▄████▀    ▀██████▀   ▀██████▀  █████▄▄██ ";
+echo "                                         ▀                                                                            ▀         ";
+
+echo -e "${BLUE}                                    https://github.com/Z4nzu/hackingtool ${NC}"
+echo -e "${RED}                                     [!] This Tool Must Run As ROOT [!]${NC}\n"
+echo -e ${CYAN}                "Select Best Option : \n"
+echo -e "${WHITE}              [1] Kali Linux / Parrot-Os (apt)"
+echo -e "${WHITE}              [2] Arch Linux (pacman)" # added arch linux support because of feature request #231
+echo -e "${WHITE}              [0] Exit "
+echo -n -e "Z4nzu >> "
+read choice
+INSTALL_DIR="/usr/share/doc/hackingtool"
+BIN_DIR="/usr/bin/"
+if [ $choice == 1 ] || [ $choice == 2 ]; then
+	echo "[*] Checking Internet Connection .."
+	wget -q --tries=10 --timeout=20 --spider https://google.com
+	if [[ $? == 0 ]]; then
+        echo -e ${BLUE}"[✔] Loading ... "
+        if [ $choice == 1 ]; then
+            sudo apt-get update -y && apt-get upgrade -y
+            sudo apt-get install python3-pip -y
+        elif [ $choice == 2 ]; then # added arch linux support because of feature request #231
+            sudo pacman -Suy
+            sudo pacman -S python-pip yay
+        fi
+
+	    echo "[✔] Checking directories..."
+	    if [ -d "$INSTALL_DIR" ]; then
+	        echo "[!] A Directory hackingtool Was Found.. Do You Want To Replace It ? [y/n]:" ;
+	        read input
+	        if [ "$input" = "y" ]; then
+	            sudo rm -R "$INSTALL_DIR"
+	        else
+	            exit
+	        fi
+	    fi
+
+        echo "[✔] Installing ...\n";
+        sudo git clone https://github.com/Z4nzu/hackingtool.git "$INSTALL_DIR";
+        echo "#!/bin/bash
+        python3 $INSTALL_DIR/hackingtool.py" '${1+"$@"}' > hackingtool;
+        sudo chmod +x hackingtool;
+        sudo cp hackingtool /usr/bin/ && rm hackingtool;
+
+        echo "\n[✔] Trying to installing Requirements ..."
+        if [ $choice == 1 ]; then
+            sudo pip3 install lolcat boxes flask requests
+            sudo apt-get install -y figlet
+        elif [ $choice == 2 ]; then # added arch linux support because of feature request #231
+            sudo pip3 install lolcat boxes flask requests
+            yay -S boxes --noconfirm
+            sudo pacman -S figlet
+        fi
+
+	else
+		  echo -e $RED "Please Check Your Internet Connection ..!!"
+	fi
+
+    if [ -d "$INSTALL_DIR" ]; then
+        echo "";
+        echo "[✔] Successfuly Installed !!! \n\n";
+        echo -e $ORANGE "       [+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]"
+        echo            "       [+]                                                             [+]"
+        echo -e $ORANGE "       [+]     ✔✔✔ Now Just Type In Terminal (hackingtool) ✔✔✔         [+]"
+        echo            "       [+]                                                             [+]"
+        echo -e $ORANGE "       [+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]"
+    else
+        echo "[✘] Installation Failed !!! [✘]";
+        exit
+    fi
+elif [ $choice == 0 ] && [ $choice != 1 ] && [ $choice != 2 ]; then # fixed the "./test.sh: line 107: [: asd: integer expression expected" when entering any invalid input containing letters
+    echo -e $RED "[✘] THank Y0u !! [✘] "
+    exit
+else
+    echo -e $RED "[!] Select Valid Option [!]"
+fi
